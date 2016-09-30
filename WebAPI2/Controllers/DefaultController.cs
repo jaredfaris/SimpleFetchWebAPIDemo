ef2ab4d961f80765ce49ee0036922ffcab6445a4
Jared Faris <jfaris@gmail.com>
2016-09-29 21:01:19 -0400

    Added sample api

---
+++ WebAPI2/Controllers/DefaultController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI2.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: api/Default
+        [Route("api/users")]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IEnumerable<User> Get()
+        {
+            return new[]
+            {
+                new User
+                {
+                    FirstName = "Bob",
+                    LastName = "Smith",
+                    Id = 1
+                },
+                new User
+                {
+                    FirstName = "Jane",
+                    LastName = "Andrews",
+                    Id = 2
+                },
+                new User
+                {
+                    FirstName = "Pat",
+                    LastName = "Jones",
+                    Id = 3
+                }
+            };
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
